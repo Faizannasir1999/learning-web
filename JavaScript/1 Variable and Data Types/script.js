1b2fd9fadc2a30aa2e92f950dc2fa5c1a0e0794a
Faizan Nasir <96426247+Faizankp@users.noreply.github.com>
2025-07-13 10:15:08 -0700

    day1

---
+++ JavaScript/1 Variable and Data Types/script.js
@@ -0,0 +1,31 @@
+
+// String
+let myString = "Hello World";
+
+// Number
+let myNumber = '123';
+
+// Boolean
+let myBoolean = true;
+
+// undefined
+let myUndefined;
+ 
+// null
+let myNull = null;
+
+
+
+console.log(myString);     
+console.log(typeof myNumber);     
+console.log(myBoolean);    
+console.log(myUndefined);  
+console.log(myNull);      
+
+const myCity = "Lahore";
+console.log(myCity);
+
+let variable = 50;
+console.log(typeof variable);
+variable = "I am now String";
+console.log(typeof variable);
