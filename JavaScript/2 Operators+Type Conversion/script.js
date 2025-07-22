dcb401dffa1f71578bd8739397dd00acb205022a
Faizan Nasir <96426247+Faizankp@users.noreply.github.com>
2025-07-22 09:22:15 -0700

    day 2 done

---
+++ JavaScript/2 Operators+Type Conversion/script.js
@@ -0,0 +1,26 @@
+// let a = 10;
+// let b = 3;
+// let c = 4;
+// a+=3+b+c;
+// console.log(a);
+
+
+// Task 1  
+let num1 = 10;
+let num2 = 5;
+
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+
+
+// String to Number
+let str = "100";
+let convertedNumber = Number(str);
+console.log("String to Number:", convertedNumber); // 100
+
+// Number to String
+let number = 200;
+let convertedString = String(number);
+console.log("Number to String:", convertedString); // "200"
