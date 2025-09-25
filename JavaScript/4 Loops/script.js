9cbea0dd212f7d7a7676b3fa3fa82d0ec1cc69a7
Faizan Nasir <kpfaizan@yahoo.com>
2025-09-25 08:53:01 -0700

    update

--- JavaScript/4 Loops/script.js
+++ JavaScript/4 Loops/script.js
@@ -15,6 +15,22 @@ for( let i = 1 ; i<=10; i++){
     console.log( num+"x" +i  + "=" +(num*i));
 }
 
+// Fizz-Buzz Challenge
 
+for (let i = 1; i<=100; i++){
+    if(i%3===0 && i%5===0)
+        console.log("Fizz-Buzz",i);
+
+else if(i%3===0){
+    console.log("Fizz",i);
+}
+else if(i%5===0){
+    console.log("Buzz",i);
+}
+else{
+    console.log(i);
+
+}
+}
 
 
