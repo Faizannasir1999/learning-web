2b658b46dc70b6b04ea4fad74c97c8c0651eac79
Faizan Nasir <kpfaizan@yahoo.com>
2025-10-22 04:13:37 -0700

    update

---
+++ JavaScript/5 Functions/function.js
@@ -0,0 +1,22 @@
+
+
+// let i = 1;
+// while (i <= 5) {
+//   console.log("Number: " + i);
+//   i++;
+// }
+
+let i = 1;
+do {
+  console.log("Number: " + i);
+  i++;
+} while (i <= 5);
+
+// now we need sum of first 10 numbers.
+
+let number = 0;
+for ( let x = 1; x<=10; x++){
+    number = number + x;
+}
+console.log(number);
+
